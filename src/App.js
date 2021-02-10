9cfc3ef93cbb1eaab8a5f5d970d3b78d5fc8ea5c
Zahra Shahid <65255043+ZahraShahid@users.noreply.github.com>
2021-02-11 02:20:15 +0500

    Leadersboard print success once the score of an individual reaches above 350

--- src/App.js
+++ src/App.js
@@ -6,7 +6,12 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Student />
+        <h1 className="lb_heading">Leadersboard</h1>
+        <Student name="Laiba Zahid" uni="Hajvery University" score={341} />
+        <Student name="Zahid Ahmed" uni="NUML" score={357} />
+        <Student name="Ahtsiham Shahid" uni="FAST NUCES" score={318} />
+        <Student name="Hadia Khan" uni="Punjab University" score={368} />
+        <Student name="Zahra Shahid" uni="PUCIT" score={289} />
       </div>
     );
   }
--- src/Student.js
+++ src/Student.js
@@ -2,17 +2,59 @@ import React, { Component } from "react";
 import "./student.css";
 
 class Student extends Component {
+  constructor() {
+    super();
+    this.state = {
+      score: 0,
+      success: false
+    };
+  }
+
+  componentDidMount() {
+    this.setState(
+      {
+        score: this.props.score
+      },
+      () => {
+        if (this.state.score >= 350) {
+          this.setState({ success: true });
+        }
+      }
+    );
+  }
+
+  addScore() {
+    this.setState({
+      score: this.state.score + 1
+    });
+  }
+
   render() {
+    const isSuccess = this.state.success;
+    let text;
+    if (isSuccess) {
+      text = <span>Success!</span>;
+    } else {
+      text = "";
+    }
+
     return (
       <div className="student">
         <div className="left">
-          <h2 className="std_name">Zahra Shahid</h2>
-          <p className="uni_name">PUCIT</p>
+          <h2 className="std_name">
+            {this.props.name}
+            <button className="add-btn" onClick={() => this.addScore()}>
+              +
+            </button>
+          </h2>
+          <p className="uni_name">
+            {this.props.uni} {text}
+          </p>
         </div>
 
         <div className="right">
           <div className="score">
-            <h2>96</h2>
+            <h2>{this.state.score}</h2>
           </div>
         </div>
       </div>
